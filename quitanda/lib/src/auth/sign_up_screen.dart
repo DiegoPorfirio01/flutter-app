df100e6ad9df1da517492cb94b99f8e872a1cdb4
bleiniak <porfiriodiego64@gmail.com>
2023-09-07 00:01:49 -0300

    feat(inputs-masks) create inputs for register user

---
+++ quitanda/lib/src/auth/sign_up_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:quitanda/src/auth/components/custom_text_field.dart';
+import 'package:quitanda/src/config/custom_colors.dart';
+
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({super.key});
+
+  final cpfFormatter = MaskTextInputFormatter(
+      mask: '###.###.###-##', filter: {"#": RegExp(r'[0-9]')});
+  final phoneFormatter = MaskTextInputFormatter(
+      mask: '## # ####-####', filter: {"#": RegExp(r'[0-9]')});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text('Cadastro',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 40),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const CustomTextField(
+                            icon: Icons.email, label: 'E-mail'),
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        const CustomTextField(
+                            icon: Icons.person, label: 'Nome'),
+                        CustomTextField(
+                            icon: Icons.phone,
+                            label: 'Celular',
+                            inputFormatters: [phoneFormatter]),
+                        CustomTextField(
+                            icon: Icons.file_copy,
+                            label: 'CPF',
+                            inputFormatters: [cpfFormatter]),
+                        SizedBox(
+                          height: 40,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Cadastrar',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
